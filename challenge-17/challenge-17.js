e768aba9d3811ddcf49ed763d9eb4375b9e924ca
Rodrigo Almeida <rodrigoalmeidagit@gmail.com>
2021-02-13 13:10:05 -0300

    correção challenge-17

--- challenge-17/challenge-17.js
+++ challenge-17/challenge-17.js
@@ -81,16 +81,31 @@ Use um console.log para cada mês, usando a frase:
 console.log( '\nMeses representados por números:' );
 
 function getMonthNumber( month ) {
-    var months = [
-        {janeiro: 01},
-        {fevereiro: 02},
-        {março: 03},
-        {setembro: 09},
-        {dezembro: 12}
-    ];
-    
+    var months = {
+        janeiro: '01',
+        fevereiro: '02',
+        março: '03',
+        abril: '04',
+        maio: '05',
+        junho: '06',
+        julho: '07',
+        agosto: '08',
+        setembro: '09',
+        outubro: '10',
+        novembro: '11',
+        dezembro: '12'
+    }
+    return months[ month ]
 }
 
+console.log('O mês de março é representado pelo número ' + 
+getMonthNumber( 'março' ) + '.' );
+
+console.log('O mês de setembro é representado pelo número ' 
++ getMonthNumber( 'setembro' ) + '.' );
+
+console.log('O mês de dezembro é representado pelo número ' 
++ getMonthNumber( 'dezembro' ) + '.' );
 
 /*
 Agora, declare uma variável chamada `regexDate` que irá receber a expressão
@@ -102,7 +117,9 @@ Mostre a regex no console.
 */
 console.log( '\nRegex que vai fazer o match com as datas do texto:' );
 
-var regexDate = 
+var regexDate = /(\d{2}) de (\w{4,}) de (\d{4})/g
+
+console.log( regexDate );
 
 /*
 Agora crie a função que irá fazer o replace dos dados. A função será chamada
@@ -112,6 +129,12 @@ Após criar a função, faça o replace das datas no texto, mostrando no
 console o resultado.
 */
 console.log( '\nReplace de datas:' );
-// ?
+
+function replaceDate( regex, dia, mes, ano ){
+    return dia + '/' + getMonthNumber( mes ) + '/' + ano;
+}
+
+console.log( text.replace( regexDate, replaceDate ) )
+
 
 })();
